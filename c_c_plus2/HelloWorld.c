17c48fabc2ca467e0acc84cad54ece7e464f9021
xiongkai <291309079@qq.com>
2019-11-19 00:30:27 +0800

    add project sources

---
+++ c_c_plus2/HelloWorld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void test(void) {
+	//Êä³ö Hello World!
+
+	/*Êä³ö Hello World!
+	Êä³ö Hello World!
+	*/
+
+	printf("Hello World!");
+
+	system("pause");
+}
---
+++ c_c_plus2/MyMain.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+	printf("Hello World!\n");
+
+	system("pause");
+
+	long long long int a = 123;
+
+	return 0;
+}
