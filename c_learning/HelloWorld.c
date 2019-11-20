810c8dc964698ac56fe033017f1df4c536983419
xiongkai <291309079@qq.com>
2019-11-20 18:47:58 +0800

    test

--- c_learning/HelloWorld.c
+++ c_learning/HelloWorld.c
@@ -11,4 +11,21 @@ void test(void) {
 	printf("Hello World!");
 
 	system("pause");
+
+for(int i = 0; i < 5; i++){
+    for (int j = 0; j < 6; ++j) {
+        if(i == 2 && j == 1){
+            goto end;
+        }
+    }
+}
+end:
+
+    double balance[10];
+
+double balance[5] = {1000.0, 2.0, 3.4, 7.0, 50.0};
+
+double balance[] = {1000.0, 2.0, 3.4, 7.0, 50.0};
+    balance[4] = 50.0;
+    double salary = balance[9];
 }
