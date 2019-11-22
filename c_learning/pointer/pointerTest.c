fadabf85a5557774fc8154df54876acd2ad1fbfd
scott <123@qq.com>
2019-11-22 17:43:58 +0800

    update

--- c_learning/pointer/pointerTest.c
+++ c_learning/pointer/pointerTest.c
@@ -6,6 +6,8 @@
 
 #include "pointerTest.h"
 
+void pointerTest2();
+
 void pointerTest(){
     int arr_a[2][2] = {{1, 2}, {3, 4}};
     int (*arr_a_pa)[2][2] = &arr_a;
@@ -29,4 +31,23 @@ void pointerTest(){
     printf("int_a = %d\n", *p_a);
     *p_a = 2000;
     printf("int_a = %d\n", *p_a);
+
+    printf("------------------pointerTest2------------------\n");
+    pointerTest2();
+}
+
+void pointerTest2(){
+    int a = 123;
+    int *p_a = &a;
+    int * *p_p_a = &p_a;
+    int * * *p_p_p_a = &p_p_a;
+    printf("%d  %d %d\n", ***p_p_p_a, **p_p_a, *p_a);
+
+    int arr_a[2] = {1, 2};
+    int *arr_p_a = arr_a;
+    printf("%d\n", arr_p_a[1]);
+
+    int int_a = 123;
+    int *p_int_a = &int_a;
+    printf("%d\n", p_int_a[1]);
 }
